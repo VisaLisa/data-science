42468d310f093e8144cef6eb6df5ae17bed6c671
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2020-06-11 12:33:08 -0500

    adding data set and updating app.py

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 import pandas as pd 
-import numpy
+import numpy as np 
+DATA_URL = (
+    "./data-science/Motor_Vehicle_Collisions_-_Crashes.csv"
+)
+
+st.title("Motor Vehicle Collisions in New York City")
+st.markdown("This application is a Steamlit dashboard that can be used"
+" to analyze motor vehicle collisions in NYC")
